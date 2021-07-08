7cff54a247e36039c62c13fbba6c15f90e059946
Soulem <ewilcher@fullsail.edu>
2021-07-07 21:50:21 -0400

    updated adapter and objectviewmodel
    updated adapter and objectviewmodel classes

--- app/src/main/java/com/dynamicdevs/ddbestiary/adapter/DisplayRecyclerViewAdapter.kt
+++ app/src/main/java/com/dynamicdevs/ddbestiary/adapter/DisplayRecyclerViewAdapter.kt
@@ -3,11 +3,12 @@ package com.dynamicdevs.ddbestiary.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.dynamicdevs.ddbestiary.databinding.BasicCardDisplayItemBinding
 import com.dynamicdevs.ddbestiary.databinding.CardDisplayItemBinding
 import com.dynamicdevs.ddbestiary.model.data.dd.monster.DDMonsterResult
 
 class DisplayRecyclerViewAdapter(): RecyclerView.Adapter<DisplayRecyclerViewAdapter.CardItemHolder>() {
-    inner class CardItemHolder(val binding: CardDisplayItemBinding): RecyclerView.ViewHolder(binding.root)
+    inner class CardItemHolder(val binding: BasicCardDisplayItemBinding): RecyclerView.ViewHolder(binding.root)
 
     var apiList: List<DDMonsterResult> = listOf()
     set(value){
@@ -16,7 +17,7 @@ class DisplayRecyclerViewAdapter(): RecyclerView.Adapter<DisplayRecyclerViewAdap
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardItemHolder {
-        val binding = CardDisplayItemBinding.inflate(
+        val binding = BasicCardDisplayItemBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
@@ -27,7 +28,7 @@ class DisplayRecyclerViewAdapter(): RecyclerView.Adapter<DisplayRecyclerViewAdap
     override fun onBindViewHolder(holder: CardItemHolder, position: Int) {
         val item = apiList[position]
         holder.binding.apply {
-            this.nameTextView.text = item.name
+            this.monsterNameTextViewBasic.text = item.name
         }
     }
 
--- app/src/main/java/com/dynamicdevs/ddbestiary/viewmodel/ObjectViewModel.kt
+++ app/src/main/java/com/dynamicdevs/ddbestiary/viewmodel/ObjectViewModel.kt
@@ -14,6 +14,9 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class ObjectViewModel(): ViewModel() {
+    companion object {
+        val instance = ObjectViewModel()
+    }
     // for a single monster look up
     val monsterLiveData = MutableLiveData<DDMonsterResult>()
     // for a list of monsters

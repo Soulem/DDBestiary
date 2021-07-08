94c8a32bdc33dd105f33d64af78e434dd799367a
anokapee <85745298+anokapee@users.noreply.github.com>
2021-07-07 23:00:00 -0400

    More clean Up before big commit

--- app/src/main/java/com/dynamicdevs/ddbestiary/view/activity/MainActivity.kt
+++ app/src/main/java/com/dynamicdevs/ddbestiary/view/activity/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity()  {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel.setDatabase(this)
-        viewModel.searchForMonster("")
-        //viewModel.getMonsters()
+        viewModel.getMonsters()
     }
 }

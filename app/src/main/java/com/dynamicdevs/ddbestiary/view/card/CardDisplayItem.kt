2718a3cd0836fe87a2d39f90fa5a1f7135676898
anokapee <85745298+anokapee@users.noreply.github.com>
2021-07-07 20:28:19 -0400

    Added intent to go to activity from card

--- app/src/main/java/com/dynamicdevs/ddbestiary/view/card/CardDisplayItem.kt
+++ app/src/main/java/com/dynamicdevs/ddbestiary/view/card/CardDisplayItem.kt
@@ -1,12 +1,26 @@
 package com.dynamicdevs.ddbestiary.view.card
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.dynamicdevs.ddbestiary.R
+import com.dynamicdevs.ddbestiary.databinding.CardDisplayItemBinding
+import com.dynamicdevs.ddbestiary.util.Constants.DND5eAPICONSTANTS.NAME_KEY
+import com.dynamicdevs.ddbestiary.view.activity.DetailedMonsterActivity
 
 class CardDisplayItem : AppCompatActivity() {
+    private lateinit var binding: CardDisplayItemBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.card_display_item)
+
+        binding = CardDisplayItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun cardClick(view: View){
+        var intent = Intent(this, DetailedMonsterActivity::class.java)
+        intent.putExtra(NAME_KEY, binding.nameTextView.text)
     }
 }
